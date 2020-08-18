e45e1b9525512baf05ed3a392d7ef1ddfd6c2ab9
Md Rana Mahmud <ranamd0@gmail.com>
2020-08-18 19:58:08 +0600

    app done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let searchInput = document.querySelector("input.form-control");
+let url = 'https://api.lyrics.ovh/suggest/';
 
 let button = document.querySelector(".search-btn");
 let searchResult = document.querySelector(".search-result");
 button.addEventListener('click', function () {
     let inputText = searchInput.value;
-    let url = 'https://api.lyrics.ovh/suggest/';
     let searchUrl = url + inputText;
     searchResult.innerHTML = "";
     fetch(searchUrl)
@@ -19,7 +19,42 @@ function showData(data) {
     data10.map(song => {
 
         showSearch(song);
-    })
+      
+    });
+
+    let lyricButton = document.querySelectorAll(".get-lyrics");
+    let results = document.querySelectorAll(".single-result");
+   
+    for(let i = 0; i < data10.length; i++){
+        let button = lyricButton[i];
+        button.addEventListener('click',function(){
+            let artist = data10[i].artist.name;
+            let title = data10[i].title;
+            //  song.artist + '/' + song.title
+            let songUrl =  'https://api.lyrics.ovh/v1/' + artist + '/' + title;
+            fetch(songUrl)
+            .then(response=>response.json())
+            .then(data =>{
+             
+                lyric = data['lyrics'];
+                if(lyric !== undefined){
+                    let lyricsHTML= document.createElement('p');
+                    lyricsHTML.innerText = lyric;
+                    lyricsHTML.className = "lyric text-white"
+                    results[i].appendChild(lyricsHTML);
+                }else{
+                    let lyricsHTML= document.createElement('p');
+                    lyricsHTML.innerText = "Lyrics Not Found";
+                    lyricsHTML.style = "color:red";
+                    results[i].appendChild(lyricsHTML);
+                }
+             
+              
+            })
+        })
+
+    }
+           
 
 }
 
@@ -33,8 +68,12 @@ function showSearch(song) {
                         <p class="author lead">Album: ${album} by <span>${artist}</span></p>
                     </div>
                     <div class="col-md-3 text-md-right text-center">
-                        <button class="btn btn-success">Get Lyrics</button>
+                        <button class="get-lyrics btn btn-success">Get Lyrics</button>
                     </div>
                 </div>`
     searchResult.insertAdjacentHTML('beforeend', result);
+
+  
+
 }
+
